cc7fb3a0c2e51db3ae1a659ae0958e838a66d180
Mitchel-DS <mitchel.staal@outlook.com>
2023-02-14 10:38:00 +0100

    finalised businesscard

--- visitekaartje/js/script.js
+++ visitekaartje/js/script.js
@@ -3,10 +3,8 @@ const bioParagraph = document.querySelector('p');
 
 const fetchStudent = async () => {
     const url = "https://whois.fdnd.nl/api/v1/member?id=cldex4djj47ty0av0n01u0gp1";
-
     const response = await fetch(url);
 	const data = await response.json();
-	// console.log(data);
 	return data;
 };
 
@@ -15,7 +13,6 @@ window.addEventListener('load', async () => {
 	const student = await fetchStudent();
 	console.log(student.member.name);
 	console.log(student.member.bio.html);
-
 	nameHeading.innerHTML = student.member.name + ' ' + student.member.surname;
 	bioParagraph.innerHTML = student.member.bio.html;
 });
